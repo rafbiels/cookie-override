7b62a70a02b42a9b7d4b119c32f9414b1aabb22a
Rafal Bielski <rafbiels@gmail.com>
2021-03-21 17:42:58 +0100

    Initial commit with code :cookie:

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# cookie-override
-Browser extension allowing to force specific cookie values whenever a website producing them is visited
+### Cookie Override browser extension
+
+The extension allows to add rules overriding specific cookie values whenever
+a website producing the cookie is visited. This allows, for example, to persist
+and force website preferences like language and location despite removing
+cookies on browser exit (which is a browser option some people concerned about
+their privacy may enable). The preferences can be stored as rules in the
+extension and applied automatically when a page is visited, while keeping the
+page unaware of the user's previous visits.
+
+----
+
+The extension icon is a derivative work based on icons made by
+[Freepik](https://www.freepik.com) from [Flaticon](https://www.flaticon.com/).
---
+++ background_scripts/background.js
@@ -0,0 +1,132 @@
+// Hard-coded rules for initial testing
+var rules = [
+  {domain:"ecosia.org", name:"ECFG", multiValueSeparator:":", subnames:["mc"], values:["en-gb"]},
+  {domain:"youtube.com", name:"PREF", multiValueSeparator:"&", subnames:["hl", "gl"], values:["en-GB", "PL"]}
+]
+
+function cookiePrintName(cookie) {
+  return cookie.name + " (" + cookie.domain + ")";
+}
+
+function getActiveTab() {
+  return browser.tabs.query({active: true, currentWindow: true});
+}
+
+function getNewCookies(currentUrl, cookieArray) {
+  let cookiesToSet = [];
+  for (ck of cookieArray) {
+    // Consider only cookies for the domain in current URL
+    if (!currentUrl.includes(ck.domain.substring(1))) continue;
+
+    for (rule of rules) {
+      // Match rules to cookies by name and domain
+      if (!ck.domain.includes(rule.domain)) continue;
+      if (!ck.name.includes(rule.name)) continue;
+
+      let newValue = ""
+      if (rule.multiValueSeparator) { // multi-value cookie rule
+        let allValues = ck.value.split(rule.multiValueSeparator);
+        let allValuesObj = {};
+        for (subValueString of allValues) {
+          let subValueStringSplit = subValueString.split('=');
+          Object.defineProperty(allValuesObj,
+                                subValueStringSplit[0],
+                                {value: subValueStringSplit[1], writable: true});
+        }
+        for (let i=0; i<rule.subnames.length; i++) {
+          Object.defineProperty(allValuesObj,
+                                rule.subnames[i],
+                                {value:  rule.values[i]});
+        }
+        for (let prop of Object.getOwnPropertyNames(allValuesObj)) {
+          let newSubValue = prop + "=" + allValuesObj[prop]
+          if (!newValue) newValue = newSubValue;
+          else newValue += rule.multiValueSeparator + newSubValue;
+        }
+      }
+      else { // single-value cookie rule
+        newValue = rule.values[0];
+      }
+      if (ck.value == newValue) {
+        // console.log("Cookie " + ck.name + " already has the desired value " + ck.value);
+        continue;
+      }
+      console.log("Change " + cookiePrintName(ck) + " value from " + ck.value + " to " + newValue);
+      ck.value = newValue;
+      cookiesToSet.push(ck);
+    }
+  }
+
+  return cookiesToSet;
+}
+
+function setCookies(currentUrl, currentTabId, cookieArray) {
+  for (ck of cookieArray) {
+    details = {
+      domain: ck.domain,
+      name: ck.name,
+      value: ck.value,
+      url: currentUrl
+    };
+    if ('expirationDate' in ck) details.expirationDate = ck.expirationDate;
+    if ('firstPartyDomain' in ck) details.firstPartyDomain = ck.firstPartyDomain;
+    if ('httpOnly' in ck) details.httpOnly = ck.httpOnly;
+    if ('path' in ck) details.path = ck.path;
+    if ('sameSite' in ck) details.sameSite = ck.sameSite;
+    if ('secure' in ck) details.secure = ck.secure;
+    if ('storeId' in ck) details.storeId = ck.storeId;
+    else details.url = ck.domain;
+
+    try {
+      browser.cookies.set(details)
+      .then( (setCookie) => {
+        console.log("Cookie " + cookiePrintName(setCookie) + " updated successfully");
+      })
+      .catch( (err) => {
+        console.error("Failed to set cookie. " + err);
+      } );
+    } catch (err) {
+      console.error("Exception caught when setting cookie. " + err);
+    }
+  }
+  if (cookieArray.length > 0) {
+    // reload the page to use the updated cookies
+    browser.tabs.reload(currentTabId);
+  }
+}
+
+function cookieOverride() {
+  getActiveTab()
+  .then((tabs) => {
+    let currentUrl = tabs[0].url;
+    let currentTabId = tabs[0].id;
+
+    // Run only for pages with rules defined
+    let runForCurrentUrl = false;
+    for (rule of rules) {
+      if (currentUrl.includes(rule.domain)) {
+        runForCurrentUrl = true;
+        break;
+      }
+    }
+    if (!runForCurrentUrl) return;
+
+    browser.cookies.getAll({})
+    .then((ca) => {
+      let cookiesArray = ca;
+      cookiesToSet = getNewCookies(currentUrl, cookiesArray);
+      setCookies(currentUrl, currentTabId, cookiesToSet);
+    })
+    .catch((err) => {
+      console.warn("Could not retrieve cookies, cookie override not possible. " + err);
+    });
+  })
+  .catch((err) => {
+    console.warn("Could not determine URL of the current active tab, cookie override not possible. " + err);
+  });
+}
+
+// update when the tab is updated
+browser.tabs.onUpdated.addListener(cookieOverride);
+// update when the tab is activated
+browser.tabs.onActivated.addListener(cookieOverride);
